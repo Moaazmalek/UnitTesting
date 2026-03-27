1021684762bf855122cf0461957f63cbb5275616
moaaz malek <madhalfrwat@gmail.com>
2026-03-28 02:49:45 +0300

    Add New Unit Tests.

--- SW.Payroll.Tests/SalarySlipProcessorTests.cs
+++ SW.Payroll.Tests/SalarySlipProcessorTests.cs
@@ -25,7 +25,90 @@ namespace SW.Payroll.Tests
             //Assert
             Assert.Equal(actual, expected);
         }
+        [Fact]
+        public void CalculateBasicSalary_ForEmployeeIsNull_ThrowArgumentNullException()
+        {
+            //AAA 
+
+            // Arrange 
+            Employee employee = null;
+
+            //Act 
+            var salarySlipProcessor = new SalarySlipProcessor(null);
+
+            Func<Employee, decimal> func = (e) => salarySlipProcessor.CalculateBasicSalary(employee);
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => func(employee));
+        }
+        [Fact]
+        public void CalculateTransportationAllowece_ForEmployeeIsNull_ThrowArgumentNullException()
+        {
+            //AAA 
+
+            // Arrange 
+            Employee employee = null;
+
+            //Act 
+            var salarySlipProcessor = new SalarySlipProcessor(null);
+
+            Func<Employee, decimal> func = (e) => salarySlipProcessor.CalculateTransportationAllowece(employee);
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => func(employee));
+        }
+        [Fact]
+        public void CalculateTransportationAllowece_ForEmployeeWorkInOffice_ReturnsTransportationAllowece()
+        {
+            //AAA 
+
+            // Arrange 
+            var employee = new Employee { WorkPlatform = WorkPlatform.Office };
+
+            //Act 
+            var salarySlipProcessor = new SalarySlipProcessor(null);
+
+            var actual = salarySlipProcessor.CalculateTransportationAllowece(employee);
+            var expected = Constants.TransportationAllowanceAmount;
+            // Assert
+            Assert.Equal(actual, expected);
+
+        }
+        [Fact]
+        public void CalculateTransportationAllowece_ForEmployeeWorkRemote_ReturnsTransportationAllowece()
+        {
+            //AAA 
+
+            // Arrange 
+            var employee = new Employee { WorkPlatform = WorkPlatform.Remote };
+
+            //Act 
+            var salarySlipProcessor = new SalarySlipProcessor(null);
+
+            var actual = salarySlipProcessor.CalculateTransportationAllowece(employee);
+            var expected = 0m;
+            // Assert
+            Assert.Equal(actual, expected);
+
+        }
+        [Fact]
+        public void CalculateTransportationAllowece_ForEmployeeWorkHybridMode_ReturnsTransportationAllowece()
+        {
+            //AAA 
+
+            // Arrange 
+            var employee = new Employee { WorkPlatform = WorkPlatform.Hybrid };
+
+            //Act 
+            var salarySlipProcessor = new SalarySlipProcessor(null);
+
+            var actual = salarySlipProcessor.CalculateTransportationAllowece(employee);
+            var expected = Constants.TransportationAllowanceAmount / 2;
+            // Assert
+            Assert.Equal(actual, expected);
+
+        }
+
       
+
 
     }
 }
